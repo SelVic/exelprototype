19f6eff9e9daa25faffbdaca806540839e0e0be1
SelVic <limelefthand@gmail.com>
2020-09-14 23:29:12 +0300

    123

---
+++ test.js
@@ -0,0 +1,7 @@
+
+// <script>
+//     (miliseconds =>{
+//     let currentTime = new Date().getTime();
+//     while (currentTime + miliseconds >= new Date().getTime()) {}
+// })(10000)
+// </script>
