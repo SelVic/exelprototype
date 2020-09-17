3ff6e2d8dfc61647c54f3eb69be9fd6197da2aec
SelVic <limelefthand@gmail.com>
2020-09-17 15:54:19 +0300

    123

---
+++ test.js
@@ -0,0 +1,44 @@
+// let t = setInterval(()=> {
+//     setTimeout(()=>{
+//     clearInterval(t);
+// },3000)
+//     console.log("Привет")
+//
+// },1000)
+
+// const a = /abc/;
+// console.log(typeof(a+1))
+
+// for (let i=0; i<=10; i++) {};
+// console.log(i)
+// let a = null;
+// if(a === undefined)
+//     console.log(true)
+
+// obj1 ={name: "Petya1", job:23}
+// obj2 = {name: "Petya", age: 23}
+// let a = {...obj1,...obj2}
+// console.log(a)
+
+// const a = [1,2,3,4,5,6,7,8,9,0]
+// //
+// // a.forEach((el,ind)=> {el+=1})
+// // console.log(a)
+
+// const object1 = {name:"Ivan",age:25}; const obj2 = object1; obj2.age +=5; console.log(object1.age)
+
+// const a= [1,2,3,4,5,6,7,8]
+// // const b = [...a.slice(0,5), 6, ...a.slice(5)]
+// // console.log(b)
+
+// const a = [1,2,3,4,5,5,4,3,2,1]
+// const b = new Set(a)
+// b.join(' ')
+// console.log(b)
+
+const promise = new Promise((resolve, reject) => setTimeout(()=> {
+    console.log(1);
+    reject("2");
+}, 1000)
+);
+promise.then((result)=> console.log(2)).catch((result)=> console.log(3))
